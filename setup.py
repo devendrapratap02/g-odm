080e5016b236a0326f11ecf6cacb8b83f21a3a5c
Devendra Pratap Singh <dpratap@zynga.com>
2021-08-24 22:50:15 +0530

    setup and requiremenents update

--- setup.py
+++ setup.py
@@ -1,38 +1,36 @@
-from setuptools import setup
-
-import os.path
+import os
 import re
 import sys
 
 try:
-    from setuptools import setup
+	from setuptools import setup
 except ImportError:
-    from distutils.core import setup
+	from distutils.core import setup
 
 if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist bdist_wheel")
-    sys.exit()
+	os.system("python setup.py sdist bdist_wheel")
+	sys.exit()
 
 
 def read(filename):
-    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+	return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
 
 version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', read('gspread/__init__.py'), re.MULTILINE).group(1)
 
 setup(
-      name="godm",
-      version=version,
-      description="Data Object Model for Google Sheet",
-      url="https://github.com/devendrapratap02/g-odm",
-      author="Devendra Pratap Singh",
-      author_email="dps.manit@gmail.com",
-      keywords=["spreadsheets", "google-spreadsheets", "object-data-model"],
-      install_requires=[
-          "gspread>=0.6.2", "oauth2client>=1.5.2"
-      ],
-      python_requires=">=3.4",
-      license="MIT",
-      packages=["godm"],
-      zip_safe=False
+	name="godm",
+	version=version,
+	description="Data Object Model for Google Sheet",
+	url="https://github.com/devendrapratap02/g-odm",
+	author="Devendra Pratap Singh",
+	author_email="dps.manit@gmail.com",
+	keywords=["spreadsheets", "google-spreadsheets", "object-data-model"],
+	install_requires=[
+		"gspread>=4.0.1"
+	],
+	python_requires=">=3.4",
+	license="MIT",
+	packages=["godm"],
+	zip_safe=False
 )
